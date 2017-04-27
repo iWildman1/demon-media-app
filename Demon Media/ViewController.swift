96583cf3b1cc8a7419ef52848fff20cb1c3a7e75
Daniel Wildman <dan@daniels-mbp>
2017-04-27 15:20:16 +0100

    Initial Commit

---
+++ Demon Media/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demon Media
+//
+//  Created by Daniel Wildman on 27/04/2017.
+//  Copyright © 2017 Daniel Wildman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
